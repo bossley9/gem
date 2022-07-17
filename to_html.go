e187f3227d35676490d3d50fe88561bf53206440
Sam Bossley <bossley.samuel@gmail.com>
2022-07-17 11:33:25 -0700

    implement gemini preformatted conversion

--- to_html.go
+++ to_html.go
@@ -5,11 +5,30 @@ import (
 	"strings"
 )
 
+type State int
+
+const (
+	stateDefault State = iota
+	statePreformatted
+)
+
 // converts a given string of Gemtext to basic HTML.
 func ToHTML(gemtext string) string {
 	var output strings.Builder
+	var state State = stateDefault
 
 	for _, line := range strings.Split(gemtext, "\n") {
+		if state == statePreformatted {
+			if strings.HasPrefix(line, "```") {
+				// closing preformatted line
+				output.WriteString(convertPreformattedClosing(line))
+				state = stateDefault
+			} else {
+				// preformatted line
+				output.WriteString(line + "\n")
+			}
+			continue
+		}
 
 		if len(line) == 0 {
 			// whitespace line
@@ -17,16 +36,20 @@ func ToHTML(gemtext string) string {
 		} else if strings.HasPrefix(line, "=>") {
 			// link line
 			output.WriteString(convertLink(line))
+		} else if strings.HasPrefix(line, "```") {
+			// opening preformatted line
+			state = statePreformatted
+			output.WriteString(convertPreformattedOpening(line))
 		} else {
 			// (default) text line
 			output.WriteString("<p>" + line + "</p>")
 		}
-
 	}
 
 	return output.String()
 }
 
+// converts a given Gemtext link to HTML
 func convertLink(line string) string {
 	output := "<p>"
 	link := strings.TrimSpace(strings.TrimPrefix(line, "=>"))
@@ -64,4 +87,21 @@ func convertLink(line string) string {
 
 	output = output + "</p>"
 	return output
+}
+
+// converts a given opening preformatted Gemtext line to HTML
+func convertPreformattedOpening(line string) string {
+	output := "<figure>"
+
+	alt := strings.TrimSpace(strings.TrimPrefix(line, "```"))
+	if len(alt) > 0 {
+		output = output + "<figcaption>" + alt + "</figcaption>"
+	}
+
+	output = output + "<pre><code>"
+	return output
+}
+
+func convertPreformattedClosing(line string) string {
+	return "</code></pre></figure>"
 }
--- to_html_test.go
+++ to_html_test.go
@@ -51,3 +51,33 @@ func TestToHTML_ConvertConsecutiveLinks(t *testing.T) {
 
 	assertEqual(t, test, ref)
 }
+
+func TestToHTML_ConvertPreformatted(t *testing.T) {
+	src := "```\n" + `
+=> example.com this will not be converted
+
+Hello! Whitespace    is preserved here
+<p>fake html tags</p>
+` + "```"
+	test := ToHTML(src)
+	ref := `<figure><pre><code>
+=> example.com this will not be converted
+
+Hello! Whitespace    is preserved here
+<p>fake html tags</p>
+</code></pre></figure>`
+
+	assertEqual(t, test, ref)
+}
+
+func TestToHTML_ConvertPreformattedAltText(t *testing.T) {
+	src := "```this is alt text\n" + `
+some arbitrary code content
+` + "```"
+	test := ToHTML(src)
+	ref := `<figure><figcaption>this is alt text</figcaption><pre><code>
+some arbitrary code content
+</code></pre></figure>`
+
+	assertEqual(t, test, ref)
+}
